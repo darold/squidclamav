46638060486a267e29307a1adacb439d82e807b4
Darold Gilles <gilles@darold.net>
2016-01-18 09:12:42 +0100

    Update copyrights

--- src/squidclamav.c
+++ src/squidclamav.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2005-2015 Gilles Darold
+ *  Copyright (C) 2005-2016 Gilles Darold
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
