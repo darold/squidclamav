b15a932d964cb0ce6a887281fd68aa0e49794f77
Louis van Belle <louis@van-belle.nl>
2019-11-06 13:11:20 +0100

    Added d/build-orig.sh, which allows a simple building on Debian/Ubuntu related systems

---
+++ debian/build-orig.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+PACKAGENAME_DEBIAN="c-icap-module-squidclamav"
+PACKAGENAME_ORIGINAL="squidclamav"
+
+GIT_URL="git://github.com/darold/${PACKAGENAME_ORIGINAL}.git"
+
+if [ ! -z "${GIT_URL}" ]
+then
+    USE_GIT_URL="${GIT_URL}"
+else
+    echo "Error: unable to detect GIT_URL variable"
+    exit 1
+fi
+
+STARTPATH="$(pwd)"
+SQUIDCLAMAVTMP="$(mktemp -d)/"
+
+pushd "${SQUIDCLAMAVTMP}/"
+
+if [ -d $GIT_URL/.bzr ]; then
+    bzr co --lightweight $USE_GIT_URL $SQUIDCLAMAVTMP
+else
+    git clone --depth 1 $USE_GIT_URL $SQUIDCLAMAVTMP
+fi
+
+# Detect Package variable
+PACKAGE_VER="$(sed -n -e 's/^\(.*\)\(version \)\(.*\)$/\3/p' ChangeLog |head -n1)"
+
+if [ ! -z "${PACKAGE_VER}" ]
+then
+    git archive --format=tar --prefix="${PACKAGENAME_DEBIAN}"-"${PACKAGE_VER}/" master | gzip > "${STARTPATH}"/"${PACKAGENAME_DEBIAN}"_"${PACKAGE_VER}".orig.tar.gz
+else
+    echo "Error: Unable to detect version number, please adjust the PACKAGE_VER string."
+    exit 1
+fi
+
+echo "1 Test"
+rm -rf $SQUIDCLAMAVTMP
+
+cd $STARTPATH
+
+tar -xf ${PACKAGENAME_DEBIAN}_${PACKAGE_VER}.orig.tar.gz
+
+if [ -d $PACKAGENAME_DEBIAN-$PACKAGE_VER/ ]
+then
+    echo " ---------------------- "
+    echo "To build the debian packages, run the following: "
+    echo "cd $PACKAGENAME_DEBIAN-$PACKAGE_VER/"
+    echo "dpkg-buildpackage -uc -us -sa"
+    echo " ---------------------- "
+else
+    echo "Unable find folder $PACKAGENAME_DEBIAN-$PACKAGE_VER, did something go wrong?"
+    exit 1
+fi
+
+exit 0
